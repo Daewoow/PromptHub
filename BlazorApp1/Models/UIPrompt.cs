fa02b60e06eb3d257baaf5353085bf44cc2ef478
ORGIsEvil <vasya.cherny.2006@mail.ru>
2024-12-15 17:11:02 +0500

    some auth fixes

--- BlazorApp1/Models/UIPrompt.cs
+++ BlazorApp1/Models/UIPrompt.cs
@@ -5,9 +5,9 @@ namespace BlazorApp1.Models;
 public class UIPrompt
 {
     public int PromptId { get; set; }
+    public string NameOfUser { get; set; }
     [Required]
     public string NameOfPrompt { get; set; }
-    public string NameOfUser { get; set; }
     [Required]
     public string Description { get; set; }
     public DateTime TimeOfUpdate { get; set; }
